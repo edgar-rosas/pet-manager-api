16656719c31c77ff90ad8307c51f8470897d63a4
Edgar Rosas <me@edgarrosas.dev>
2024-07-26 12:21:57 -0700

    postgres connection

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,7 +2,11 @@ import { defineConfig, LoadStrategy } from "@mikro-orm/postgresql";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 
 export default defineConfig({
-  dbName: "pets",
+  dbName: process.env.POSTGRES_DB ?? "pet_manager",
+  host: process.env.POSTGRES_HOST ?? "localhost",
+  port: Number(process.env.POSTGRES_PORT) ?? 5432,
+  user: process.env.POSTGRES_USER ?? "postgres",
+  password: process.env.POSTGRES_PASSWORD ?? "password",
   entities: ["dist/**/*.entity.js"],
   entitiesTs: ["src/**/*.entity.ts"],
   metadataProvider: TsMorphMetadataProvider,
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from "./app.js";
 
 try {
-  const { url } = await bootstrap(Number(process.env.NODE_PORT));
+  const { url } = await bootstrap(Number(process.env.PORT));
   console.log(`Server is now running at ${url}`);
 } catch (e) {
   console.error(e);
