6e4a53d663d9297341fbfc057519cddaf5b6cf8e
Edgar Rosas <me@edgarrosas.dev>
2024-07-26 18:54:34 -0700

    hot reload and debugging

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { bootstrap } from "./app.js";
 
 try {
   const { url } = await bootstrap(Number(process.env.PORT));
-  console.log(`Server is now running at ${url}`);
+  console.log(`Server now running at ${url}`);
 } catch (e) {
   console.error(e);
 }
