9403d4e6e299aa7852318833935f3f8e61f49a31
Edgar Rosas <me@edgarrosas.dev>
2024-07-29 16:06:09 -0700

    owners endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,36 @@
-import { RequestContext} from "@mikro-orm/core";
-import {fastify} from "fastify";
-import {initORM} from "./db.js";
+import { RequestContext } from "@mikro-orm/core";
+import { fastify } from "fastify";
+import { initORM } from "./db.js";
 
 export async function bootstrap(port = 3000) {
-    const db = await  initORM();
-    const app = fastify();
+  const db = await initORM();
+  const app = fastify();
 
-    app.addHook("onRequest", (_request, _reply, done) => {
-        RequestContext.create(db.em, done);
-    });
+  app.addHook("onRequest", (_request, _reply, done) => {
+    RequestContext.create(db.em, done);
+  });
 
-    app.addHook("onClose", async () => {
-        await db.orm.close();
-    });
+  app.addHook("onClose", async () => {
+    await db.orm.close();
+  });
 
-    app.get("/", (_request, reply) => {
-        reply.send("Hello!");
-    })
+  app.get("/", (_request, reply) => {
+    reply.send("Hello!");
+  });
 
-    app.get('/pets', async () => {
-        const [pets, total] = await db.pet.findAndCount({});
+  app.get("/pets", async () => {
+    const [pets, total] = await db.pet.findAndCount({});
 
-        return { pets, total };
-    });
+    return { pets, total };
+  });
 
-    const url = await app.listen({port, host: '0.0.0.0'});
+  app.get("/owners", async () => {
+    const [owners, total] = await db.owner.findAndCount({});
 
-    return {app, url};
+    return { owners, total };
+  });
+
+  const url = await app.listen({ port, host: "0.0.0.0" });
+
+  return { app, url };
 }
